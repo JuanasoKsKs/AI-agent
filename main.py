8e1119ca6d9f206410e88671c51e635e2d5895bd
JuanasoKsKs <jesse.bmz16@gmail.com>
2025-10-19 09:51:24 -0600

    Adition of calculator files and update on main.py

--- main.py
+++ main.py
@@ -1,6 +1,44 @@
-def main():
-    print("Hello from aiagent!")
+import os
+from dotenv import load_dotenv
+from google import genai
+import sys
+from google.genai import types
 
+
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+
+def main():
+    statement = sys.argv
+    print("Hello from aiagent!")
+    if len(sys.argv) < 2:
+        print("Tool usage: uv run main.py <prompt>")
+        print("Error: missing prompt", file=sys.stderr)
+        sys.exit(1)
+        return
+
+    user_prompt = statement[1]
+
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    response = client.models.generate_content(
+    model='gemini-2.0-flash-001', contents=messages
+    )
+    um = response.usage_metadata
+    if len(statement) > 2:
+        if statement[2] == "--verbose":
+            print("User prompt:", user_prompt)
+            print("Prompt tokens:", um.prompt_token_count)
+            print("Response tokens:", um.candidates_token_count)
+
+
+    print(response.text)
 
 if __name__ == "__main__":
     main()
